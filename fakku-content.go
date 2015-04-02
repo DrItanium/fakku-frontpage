11f4907c4c8dd15a36e7cec6a3d2ea758ad2c08d
Joshua Scoggins <Theoretically.x64@gmail.com>
2015-04-02 00:28:33 -0700

    Updated fakku-content to support topics and *Content

--- fakku-content.go
+++ fakku-content.go
@@ -18,23 +18,44 @@ func main() {
 	for i := 0; i < int(posts.Total); i++ {
 		tmp := posts.Index[i]
 		switch tmp.(type) {
+		case *fakku.Content:
+			content := tmp.(*fakku.Content)
+			printEntry(content)
 		case fakku.Content:
 			content := tmp.(fakku.Content)
-			tags := content.Tags
-			fmt.Printf("%s - ", content.Name)
-			// print out the tags one after another in a form that can be easily
-			// grepped through
-			if len(tags) == 0 {
-				fmt.Printf("No tags!")
+			printEntry(&content)
+		case fakku.Topic:
+			topic := tmp.(fakku.Topic)
+			url, err := topic.Url()
+			if err != nil {
+				fmt.Errorf("ERROR: %s", err.Error())
 			} else {
-				fmt.Printf("{ %s", tags[0].Attribute)
-				for j := 1; j < len(tags); j++ {
-					fmt.Printf(", %s", tags[j].Attribute)
-				}
-				fmt.Printf(" }")
+				fmt.Printf("Topic: %s - %s", topic.Title, url.String())
 			}
-			fmt.Printf(" - %s", content.Url)
 			fmt.Println()
 		}
 	}
+}
+func printEntry(content *fakku.Content) {
+	tags := content.Tags
+	fmt.Printf("%s - ", content.Name)
+	// print out the tags one after another in a form that can be easily
+	// grepped through
+	if len(tags) == 0 {
+		fmt.Printf("No tags!")
+	} else {
+		fmt.Printf("{ %s", tags[0].Attribute)
+		for j := 1; j < len(tags); j++ {
+			fmt.Printf(", %s", tags[j].Attribute)
+		}
+		fmt.Printf(" }")
+	}
+	url, err := content.Url()
+	if err != nil {
+		fmt.Printf(" - %s", content.RawUrl)
+	} else {
+		fmt.Printf(" - %s", url.String())
+	}
+	fmt.Println()
+
 }
